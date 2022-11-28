9ab564ae11ea1e26e7902f132cdaecad191e8d93
ztx01001 <ztx01001@163.com>
2022-11-28 12:07:08 +0800

    增加注释说明

--- src/main/java/com/zksp/hik/camera/HiKangApplication.java
+++ src/main/java/com/zksp/hik/camera/HiKangApplication.java
@@ -2,6 +2,7 @@ package com.zksp.hik.camera;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//test is ok
 
 @SpringBootApplication
 public class HiKangApplication {
